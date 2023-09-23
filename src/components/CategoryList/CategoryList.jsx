7908bc1633e37c4328911ddf5397ca3489aae4f3
mahizaman490 <mahizaman490@gmail.com>
2023-09-23 08:59:55 +0600

    created Banner and Category  component

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+
+
+const CategoryList = () => {
+    return (
+        <div>
+            <h1 className="text-6xl text-center">Job Category List </h1>
+            <p>Explore thousands of job oppertunities with all the information you need. its your future</p>
+        </div>
+    );
+};
+
+export default CategoryList;
